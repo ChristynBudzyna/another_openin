9c5b2fee53fbcf4aca0e62cc9116b8430209f66a
Christyn Budzyna <cbudzyna@gmail.com>
2016-06-21 18:31:23 -0400

    add styling to show index page

--- app/assets/javascripts/components/showpreview.js.jsx
+++ app/assets/javascripts/components/showpreview.js.jsx
@@ -1,12 +1,15 @@
 var ShowPreview = React.createClass({
   render: function(){
   return( <div className="show-profile-container">
-          <p><a href={"/shows/" + this.props.data.id}>{this.props.data.title}</a></p>
-          <p>Composer: {this.props.data.composer}</p>
-          <p>Lyricist: {this.props.data.lyricist}</p>
-          <p>Librettist: {this.props.data.librettist}</p>
-
-      </div>)
-}
-
+            <div className="panel panel-default show-panel">
+              <div className="panel-heading show-panel-headings">
+                <a href={"/shows/" + this.props.data.id}>{this.props.data.title}</a>
+              </div>
+              <div className="panel-body">
+              <p>{this.props.data.description}</p>
+              <a href={"/shows/" + this.props.data.id}><button className="btn btn-default">Learn More</button></a>
+              </div>
+            </div>
+          </div>)
+  }
 })
--- app/assets/javascripts/components/showsearch.js.jsx
+++ app/assets/javascripts/components/showsearch.js.jsx
@@ -4,65 +4,65 @@ var ShowSearch = React.createClass({
       <form action="/shows/search" method="get">
        <h2>preferred cast size</h2>
   <fieldset>
-    <input type="radio" name="cast_size" value="small" />
-      small
-    <input type="radio" name="cast_size" value="medium" />
-      medium
-    <input type="radio" name="cast_size" value="large"/>
-      large
+    <label className="radio-inline" value="small"><input type="radio" name="cast_size" value="small" />small</label>
+    <label className="radio-inline"><input type="radio" name="cast_size" value="medium" />
+      medium</label>
+    <label className="radio-inline"><input type="radio" name="cast_size" value="large"/>
+      large</label>
   </fieldset>
 
  <h2>preferred show length</h2>
   <fieldset>
-    <input type="radio" name="length" value="full length"/>
-      full length
-    <input type="radio" name="length" value="one-act"/>
-      one act
-    <input type="radio" name="length" value="ten minute"/>
-      ten minute
+    <label className="radio-inline"><input type="radio" name="length" value="full length"/>
+      full length</label>
+    <label className="radio-inline"><input type="radio" name="length" value="one-act"/>
+      one act</label>
+   <label className="radio-inline"><input type="radio" name="length" value="ten minute"/>
+      ten minute</label>
   </fieldset>
 
  <h2>book musical or revue?</h2>
  <fieldset>
-    <input type="radio" name="show_type" value="book musical"/>
-      book musical
-    <input type="radio" name="show_type" value="revue"/>
-      revue
+    <label className="radio-inline"><input type="radio" name="show_type" value="book musical"/>
+      book musical</label>
+    <label className="radio-inline"><input type="radio" name="show_type" value="revue"/>
+      revue</label>
   </fieldset>
 
  <h2>age of actors</h2>
  <fieldset>
-    <input type="radio" name="age_of_actors" value="children"/>
-      children
-    <input type="radio" name="age_of_actors" value="adults"/>
-      adults
+    <label className="radio-inline"><input type="radio" name="age_of_actors" value="children"/>
+      children</label>
+    <label className="radio-inline"><input type="radio" name="age_of_actors" value="adults"/>
+      adults</label>
   </fieldset>
 
  <h2>musical genre</h2>
   <fieldset>
-    <input type="radio" name="music_genre" value="classic musical theatre"/>
-      classic musical theatre
-    <input type="radio" name="music_genre" value="contemporary musical theatre"/>
+    <label className="radio-inline"><input type="radio" name="music_genre" value="classic musical theatre"/>
+      classic musical theatre</label>
+    <label className="radio-inline"><input type="radio" name="music_genre" value="contemporary musical theatre"/>
       contemporary musical theatre
-    <input type="radio" name="music_genre" value="pop/rock"/>
-      pop/rock
-    <input type="radio" name="gender_balance" value="rap/hip hop"/>
-      rap/hip hop
-    <input type="radio" name="gender_balance" value="operetta"/>
-      operetta
-    <input type="radio" name="gender_balance" value="other"/>
-      other
+      </label>
+    <label className="radio-inline"><input type="radio" name="music_genre" value="pop/rock"/>
+      pop/rock</label>
+    <label className="radio-inline"><input type="radio" name="gender_balance" value="rap/hip hop"/>
+      rap/hip hop</label>
+    <label className="radio-inline"><input type="radio" name="gender_balance" value="operetta"/>
+      operetta</label>
+    <label className="radio-inline"><input type="radio" name="gender_balance" value="other"/>
+      other</label>
   </fieldset>
 
    <h2>dance heavy?</h2>
   <fieldset>
-   <input type="radio" name="dance_heavy" value="true"/>
-      yes
-    <input type="radio" name="dance_heavy" value="false"/>
-    no
+   <label className="radio-inline"><input type="radio" name="dance_heavy" value="true"/>
+      yes</label>
+    <label className="radio-inline"><input type="radio" name="dance_heavy" value="false"/>
+    no</label>
   </fieldset>
 
-   <input type="submit" value="find my next show" className="button"/>
+   <input type="submit" value="find my next show" className="btn btn-default"/>
       </form>
       )
   }
