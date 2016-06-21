01fd9b6abab666fbd58ce87df67ba86b619d1526
Christyn Budzyna <cbudzyna@gmail.com>
2016-06-21 13:33:31 -0400

    Fix links and delete unneeded files

--- app/assets/javascripts/components/showpreview.js.jsx
+++ app/assets/javascripts/components/showpreview.js.jsx
@@ -1,11 +1,10 @@
 var ShowPreview = React.createClass({
   render: function(){
   return( <div className="show-profile-container">
-          <p>{this.props.data.title}</p>
+          <p><a href={"/shows/" + this.props.data.id}>{this.props.data.title}</a></p>
           <p>Composer: {this.props.data.composer}</p>
           <p>Lyricist: {this.props.data.lyricist}</p>
           <p>Librettist: {this.props.data.librettist}</p>
-          <p>Description: {this.props.data.description}</p>
 
       </div>)
 }
